6dc0b4e90560217fcb569ccbd1c688e466dee28e
AndreaSletta <70203798+AndreaSletta@users.noreply.github.com>
2021-05-13 15:16:16 +0200

    Sorting bar fixed, "old" replaced with "populare"

--- script/apiallposts.js
+++ script/apiallposts.js
@@ -3,12 +3,12 @@ const url =
 
 //Container
 const postConatiner = document.querySelector("#all");
-const containerOld = document.querySelector("#old");
+const containerPopular = document.querySelector("#popular");
 const containerFeatured = document.querySelector("#featured");
 
 //Buttons
 const showMoreButton = document.querySelector(".blackbutton");
-const sortOld = document.querySelector(".sortold");
+const sortPopular = document.querySelector(".sortpopular");
 const sortFeatured = document.querySelector(".sortfeatured");
 const sortStandard = document.querySelector(".sortstandard");
 
@@ -17,66 +17,69 @@ async function getAllPosts() {
     const response = await fetch(url);
     const content = await response.json();
 
-    containerOld.style.display = "none";
+    containerPopular.style.display = "none";
     containerFeatured.style.display = "none";
+    sortStandard.style.backgroundColor = "white";
 
-    // Standard
-    /*
-  if (window.innerWidth < 768) {
-   
-    } else if (window.innerWidth > 767 && window.innerWidth < 1024) {
-    
-    } else if (window.innerWidth > 1023) {
-      
-    }
-      */
+    //Default
     if (window.innerWidth < 1024) {
       for (var i = 0; i < 6; i++) {
         postConatiner.innerHTML += `
-        <a href="individualpost.html?id=${content[i].id}">
-          <div id="${"post" + content[i].id}" class="hoverscale">
-          <img src="${content[i].images[0].src}" alt="${content[i].images.alt}">
-        <h3>${content[i].name}</h3>  
-        <p>${content[i].short_description.slice(3, 40)} ... 
-        <p class="greentext">Continue reading
-      `;
+      <a href="individualpost.html?id=${content[i].id}">
+        <div id="${"post" + content[i].id}" class="hoverscale">
+        <img src="${content[i].images[0].src}" alt="${content[i].images.alt}">
+      <h3>${content[i].name}</h3>  
+      <p>${content[i].short_description.slice(3, 40)} ... 
+      <p class="greentext">Continue reading
+    `;
       }
     } else if (window.innerWidth > 1023) {
       for (var i = 0; i < 8; i++) {
         postConatiner.innerHTML += `
+      <a href="individualpost.html?id=${content[i].id}">
+        <div id="${"post" + content[i].id}" class="hoverscale">
+        <img src="${content[i].images[0].src}" alt="${content[i].images.alt}">
+      <h3>${content[i].name}</h3>  
+      <p>${content[i].short_description.slice(3, 40)} ... 
+      <p class="greentext">Continue reading
+    `;
+      }
+    }
+
+    //hide category dropdown
+
+    //Sort by popular
+
+    sortPopular.onclick = function showPopular() {
+      for (var i = 0; i < content.length; i++) {
+        var categories = content[i].categories;
+        for (var j = 0; j < categories.length; j++) {
+          if (categories[j].name === "Popular") {
+            containerPopular.innerHTML += `
         <a href="individualpost.html?id=${content[i].id}">
           <div id="${"post" + content[i].id}" class="hoverscale">
           <img src="${content[i].images[0].src}" alt="${content[i].images.alt}">
         <h3>${content[i].name}</h3>  
         <p>${content[i].short_description.slice(3, 40)} ... 
         <p class="greentext">Continue reading
-      `;
+        `;
+          }
+        }
       }
-    }
 
-    // Sort by old
-    sortOld.onclick = function showOld() {
-      for (var i = 0; i < 9; i++) {
-        containerOld.innerHTML += `
-        <a href="individualpost.html?id=${content.reverse()[i].id}">
-          <div id="${"post" + content.reverse()[i].id}" class="hoverscale">
-          <img src="${content.reverse()[i].images[0].src}" alt="${
-          content.reverse()[i].images.alt
-        }">
-        <h3>${content.reverse()[i].name}</h3>  
-        <p>${content.reverse()[i].short_description.slice(3, 40)} ... 
-        <p class="greentext">Continue reading
-      `;
-      }
       postConatiner.style.display = "none";
-      containerOld.style.display = "grid";
+      containerPopular.style.display = "grid";
       containerFeatured.style.display = "none";
       postConatiner.innerHTML = "";
       containerFeatured.innerHTML = "";
       showMoreButton.style.display = "none";
+      sortStandard.style.backgroundColor = "#ebedea";
+      sortFeatured.style.backgroundColor = "#ebedea";
+      sortPopular.style.backgroundColor = "white";
     };
 
-    // Sort by featured
+    //Sort by featured
+
     sortFeatured.onclick = function showFeatured() {
       for (var i = 0; i < content.length; i++) {
         var categories = content[i].categories;
@@ -95,64 +98,95 @@ async function getAllPosts() {
       }
 
       postConatiner.style.display = "none";
-      containerOld.style.display = "none";
+      containerPopular.style.display = "none";
       containerFeatured.style.display = "grid";
       postConatiner.innerHTML = "";
-      containerOld.innerHTML = "";
+      containerPopular.innerHTML = "";
+      showMoreButton.style.display = "none";
+      sortStandard.style.backgroundColor = "#ebedea";
+      sortPopular.style.backgroundColor = "#ebedea";
+      sortFeatured.style.backgroundColor = "white";
     };
 
-    // Sort by standard
+    // Sort by default
     sortStandard.onclick = function showStandard() {
-      for (var i = 0; i < 9; i++) {
-        postConatiner.innerHTML += `
-      <a href="individualpost.html?id=${content[i].id}">
-        <div id="${"post" + content[i].id}" class="hoverscale">
-        <img src="${content[i].images[0].src}" alt="${content[i].images.alt}">
-      <h3>${content[i].name}</h3>  
-      <p>${content[i].short_description.slice(3, 40)} ... 
-      <p class="greentext">Continue reading
-    `;
+      if (window.innerWidth < 1024) {
+        for (var i = 0; i < 6; i++) {
+          postConatiner.innerHTML += `
+        <a href="individualpost.html?id=${content[i].id}">
+          <div id="${"post" + content[i].id}" class="hoverscale">
+          <img src="${content[i].images[0].src}" alt="${content[i].images.alt}">
+        <h3>${content[i].name}</h3>  
+        <p>${content[i].short_description.slice(3, 40)} ... 
+        <p class="greentext">Continue reading
+      `;
+        }
+      } else if (window.innerWidth > 1023) {
+        for (var i = 0; i < 8; i++) {
+          postConatiner.innerHTML += `
+        <a href="individualpost.html?id=${content[i].id}">
+          <div id="${"post" + content[i].id}" class="hoverscale">
+          <img src="${content[i].images[0].src}" alt="${content[i].images.alt}">
+        <h3>${content[i].name}</h3>  
+        <p>${content[i].short_description.slice(3, 40)} ... 
+        <p class="greentext">Continue reading
+      `;
+        }
       }
       postConatiner.style.display = "grid";
-      containerOld.innerHTML = "";
-      containerOld.style.display = "none";
+      containerPopular.style.display = "none";
       containerFeatured.style.display = "none";
       containerFeatured.innerHTML = "";
+      containerPopular.innerHTML = "";
       showMoreButton.style.display = "block";
+      sortPopular.style.backgroundColor = "#ebedea";
+      sortFeatured.style.backgroundColor = "#ebedea";
+      sortStandard.style.backgroundColor = "white";
     };
 
-    //show more
     showMoreButton.onclick = function showMore() {
-      if ((postConatiner.style.innerHTML = "grid")) {
-        for (var i = 9; i < 14; i++) {
+      //Default
+      if (
+        window.innerWidth < 1024 &&
+        (postConatiner.style.innerHTML = "grid")
+      ) {
+        const childCount = postConatiner.childElementCount;
+        for (var i = childCount; i < childCount + 3; i++) {
           postConatiner.innerHTML += `
-          <a href="individualpost.html?id=${content[i].id}">
-            <div id="${"post" + content[i].id}" class="hoverscale">
-            <img src="${content[i].images[0].src}" alt="${
-            content[i].images.alt
-          }">
-          <h3>${content[i].name}</h3>  
-          <p>${content[i].short_description.slice(3, 40)} ... 
-          <p class="greentext">Continue reading
-        `;
+        <a href="individualpost.html?id=${content[i].id}">
+          <div id="${"post" + content[i].id}" class="hoverscale">
+          <img src="${content[i].images[0].src}" alt="${content[i].images.alt}">
+        <h3>${content[i].name}</h3>  
+        <p>${content[i].short_description.slice(3, 40)} ... 
+        <p class="greentext">Continue reading
+      `;
+          console.log(childCount);
+          if (childCount > 11) {
+            showMoreButton.style.display = "none";
+          }
         }
-        showMoreButton.style.display = "none";
-      } /*
-      if ((containerOld.style.innerHTML = "grid")) {
-        for (var i = 9; i < 14; i++) {
-          containerOld.innerHTML += `
-      <a href="individualpost.html?id=${content.reverse()[i].id}">
-        <div id="${"post" + content.reverse()[i].id}">
-        <img src="${content.reverse()[i].images[0].src}" alt="${
-            content.reverse()[i].images.alt
+      } else if (
+        window.innerWidth > 1023 &&
+        (postConatiner.style.innerHTML = "grid")
+      ) {
+        const childCount = postConatiner.childElementCount;
+        for (var i = childCount; i < childCount + 4; i++) {
+          postConatiner.innerHTML += `
+        <a href="individualpost.html?id=${content.reverse[i].id}">
+          <div id="${"post" + content.reverse[i].id}" class="hoverscale">
+          <img src="${content.reverse[i].images[0].src}" alt="${
+            content.reverse[i].images.alt
           }">
-      <h3>${content.reverse()[i].name}</h3>  
-      <p>${content.reverse()[i].short_description.slice(3, 40)} ... 
-      <p class="greentext">Continue reading
-    `;
-          showMoreButton.style.display = "none";
+        <h3>${content.reverse[i].name}</h3>  
+        <p>${content.reverse[i].short_description.slice(3, 40)} ... 
+        <p class="greentext">Continue reading
+      `;
+          console.log(childCount);
+          if (childCount > 11) {
+            showMoreButton.style.display = "none";
+          }
         }
-      } */
+      }
     };
   } catch (error) {
     console.log(error);
