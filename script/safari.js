0c15c3234377fe7f333a125ca97a73e0b9479e6e
AndreaSletta <70203798+AndreaSletta@users.noreply.github.com>
2021-05-14 12:20:19 +0200

    smooth scroll added?

--- script/safari.js
+++ script/safari.js
@@ -4,7 +4,6 @@ let height = window.innerHeight * 0.01;
 document.documentElement.style.setProperty("--vh", `${height}px`);
 
 // smooth scroll that also works on safari
-
 $(document).ready(function () {
   $("a").on("click", function (event) {
     if (this.hash !== "") {
